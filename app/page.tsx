3084bb56c83a46b1aea43e1587fa1ec26d4936f0
aditya <niu_33252@niu.edu.in>
2025-12-09 17:58:26 +0530

    ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-  return (
-    <div className='h-screen w-full '>
-      
-    </div>
-  )
+import { BackgroundPaths } from './(components)/background-paths';
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+
+
+const Hero = () => {
+
+
+    useGSAP(() => {
+
+        gsap.from(".h1", {
+            opacity: 0,
+            duration: 4,
+            ease: "slow(0.7,0.7,false)",
+        })
+        gsap.from(".p", {
+            delay: .5,
+            opacity: 0,
+            duration: 4,
+            ease: "slow(0.7,0.7,false)",
+        })
+        gsap.from(".h2 ", {
+            delay: 1,
+            opacity: 0,
+            duration: 4,
+            ease: "slow(0.7,0.7,false)",
+            stagger: .3
+        })
+
+    }, [])
+
+    return (
+        <div className='h-screen w-full'>
+            <BackgroundPaths title="DocExtract!" />
+            
+        </div>
+    )
 }
 
-export default page
+export default Hero
